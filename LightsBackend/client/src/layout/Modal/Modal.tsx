4acc34d9a062daee06fe477bb5c9dc866b9e5775
pdavid-i <popescu.david.ioan@gmail.com>
2024-01-30 09:41:56 +0200

    word modal set structure to handle usage check

--- LightsBackend/client/src/layout/Modal/Modal.tsx
+++ LightsBackend/client/src/layout/Modal/Modal.tsx
@@ -4,7 +4,7 @@ import "./Modal.css";
 import { useEffect, useState } from "react";
 import {Word} from "../../types/Word";
 import agent from "../../api/agent";
-import Heart from "../../features/Heart/Heart";
+import Heart from "../../features/HeartWord/HeartWord";
 
 interface ModalProps {
     toggleWordModal: () => void;
@@ -14,6 +14,8 @@ interface ModalProps {
 const Modal = ({ toggleWordModal, wordId} : ModalProps)  => {
 
     const [word, setWord] = useState<Word>();
+    const [userInput, setUserInput] = useState('');
+    const [inputFeedback, setInputFeedback] = useState('');
 
     const riseUp = {
         hidden: {
@@ -42,20 +44,37 @@ const Modal = ({ toggleWordModal, wordId} : ModalProps)  => {
         .then(res => {
             setWord(res)
 
-            const interactionData = {
-                wordId: res.id,
-                favourite: res.favorite,
-                uses: [] 
-            };
-    
-            // Record the interaction
-            agent.WordInteractions.new(interactionData)
-                .catch(err => console.log("Error recording interaction:", err.response));
+
             
         })
         .catch(err => console.log(err.response))
     }, [])
 
+
+    const checkFeedback = () => {
+
+        const dummy = Math.floor(Math.random()*2) > 0 ? "Great succes very nice word" : "Nuh uh";
+
+        setInputFeedback(dummy)
+        if (dummy.length > 15) {
+            recordInteraction();
+        }
+
+    }
+
+    const recordInteraction = () => {
+        
+        if (!word) return;
+
+        const interactionData = {
+            wordId: word.id,
+            uses: [] 
+        };
+
+        // Record the interaction
+        agent.WordInteractions.new(interactionData)
+            .catch(err => console.log("Error recording interaction:", err.response));
+    }
 
     return (
         <Focus toggleWordModal={toggleWordModal}>
@@ -77,6 +96,16 @@ const Modal = ({ toggleWordModal, wordId} : ModalProps)  => {
                     })}
                     <p className="example"><i>{word?.examples[0]}</i></p>
                 </ul>
+
+                <input 
+                    type="text" 
+                    value={userInput} 
+                    onChange={(e) => setUserInput(e.target.value)} 
+                    placeholder="Type a sentence" 
+                />
+                <button onClick={checkFeedback}>Check</button>
+                {/* Display API response */}
+                {inputFeedback && <p>{inputFeedback}</p>}
 
             </motion.div>
         </Focus>
